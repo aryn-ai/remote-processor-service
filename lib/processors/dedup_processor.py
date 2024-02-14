8aef6aa106c2939d008ea3f3b0767b667679695c
Alex Meyer <alex@aryn.ai>
2024-02-13 16:16:03 -0800

    Add verbose output mode to dedup processor.

--- lib/processors/dedup_processor.py
+++ lib/processors/dedup_processor.py
@@ -1,4 +1,5 @@
 import re
+import sys
 import json
 from sycamore.functions.simhash import shinglesDist
 from lib.processors.processor import RequestProcessor, ResponseProcessor
@@ -8,13 +9,16 @@ from lib.search_response import SearchResponse
 class DedupResponseProcessor(ResponseProcessor):
     nulRe = re.compile(rb'\x00+$')
     
-    def __init__(self, threshold: float) -> None:
+    def __init__(self, threshold: float, verbose: int) -> None:
         super().__init__()
         self.threshold = threshold
+        self.verbose = verbose
 
     @staticmethod
     def from_config(config) -> ResponseProcessor:
-        return DedupResponseProcessor(threshold=config["threshold"])
+        t = config["threshold"]
+        v = config.get("verbose", 0)
+        return DedupResponseProcessor(threshold=t, verbose=v)
 
     @staticmethod
     def get_class_name() -> str:
@@ -33,12 +37,15 @@ class DedupResponseProcessor(ResponseProcessor):
         sketchAry = []
         for i, hit in enumerate(hitAry):
             try:
-                raw = hit.source
-                raw = self.nulRe.sub(b"", raw)  # FIXME: bug
-                obj = json.loads(raw)
-                sketch_i = obj["shingles"]  # FIXME: must ask for shingles in q
+                raw_i = hit.source
+                raw_i = self.nulRe.sub(b"", raw_i)  # FIXME: Issue #3
+                obj_i = json.loads(raw_i)
+                # !!! must ask for shingles and text_representation in query
+                sketch_i = obj_i.get("shingles")
+                text_i = obj_i.get("text_representation", "")
             except (AttributeError, KeyError):
                 sketch_i = None
+                text_i = ""
             sketchAry.append(sketch_i)
             if sketch_i is None:
                 continue
@@ -48,15 +55,47 @@ class DedupResponseProcessor(ResponseProcessor):
                 sketch_j = sketchAry[j]
                 if sketch_j is None:
                     continue
-                if shinglesDist(sketch_i, sketch_j) < self.threshold:
+                dist = shinglesDist(sketch_i, sketch_j)
+                if dist < self.threshold:
                     validAry[i] = False
+                    if self.verbose:
+                        try:
+                            raw_j = hitAry[j].source
+                            raw_j = self.nulRe.sub(b"", raw_j)  # FIXME: #3
+                            obj_j = json.loads(raw_j)
+                            text_j = obj_j.get("text_representation", "")
+                            print("DROP", dist, file=sys.stderr)
+                            print("PREV", text_j, file=sys.stderr)
+                            print("CURR", text_i, file=sys.stderr)
+                        except (AttributeError, KeyError):
+                            pass
                     break
 
         del sketchAry
         newAry = []
-        for valid, hit in zip(validAry, hitAry):
-            if valid:
-                newAry.append(hit)
+        if not self.verbose:
+            for valid, hit in zip(validAry, hitAry):
+                if valid:
+                    newAry.append(hit)
+        else:
+            for valid, hit in zip(validAry, hitAry):
+                try:
+                    raw = hit.source
+                    raw = self.nulRe.sub(b"", raw)  # FIXME: Issue #3
+                    obj = json.loads(raw)
+                    props = obj["properties"]
+                    fn = props.get("_location")
+                    if fn is None:
+                        fn = props["path"]
+                    pn = props.get("page_number", 0)
+                    name = fn + " " + str(pn)
+                except (AttributeError, KeyError):
+                    name = "None 0"
+                if valid:
+                    newAry.append(hit)
+                    print("KEEP", name, file=sys.stderr)
+                else:
+                    print("NUKE", name, file=sys.stderr)
         del resp.internal_response.hits.hits[:]
         resp.internal_response.hits.hits.extend(newAry)
         return resp
